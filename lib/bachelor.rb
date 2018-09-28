4c29703257a6f3518eb5636495697786cbe1e406
Maayan Kfir <mayankfir@gmail.com>
2018-09-28 19:00:17 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,5 +1,4 @@
 def get_first_name_of_season_winner(data, season)
-  def get_first_name_of_season_winner(data, season)
   data.each do |season_number, info|
   info.each do |details, details_data|
   if status == "winner"
